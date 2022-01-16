2a90de50d6e88bdf70e9440eb8e995c06c41e4b4
jezura777 <68075487+jezura777@users.noreply.github.com>
2022-01-16 11:11:27 +0100

    categorie

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -17,7 +17,7 @@ const Categories = () => {
 
         <div className='bg-white shadow-lg rounded-lg p-8 mb-8 pb-12'>
             <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-                Categorie
+                Kategorie
             </h3>
             {categories.map((category) => (
 
